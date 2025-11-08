c013b4b224a1bcbc585de6b6dbe7e23362315842
Flames <vibe-coder@flames.com>
2025-11-08 06:19:36 +0000

    Update project prj_129160gz - 2025-11-08T06:19:36.079Z

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './components/Contact';
 function BackgroundGlow() {
   useEffect(() => {}, []);
   return (
-    <div aria-hidden className="pointer-events-none fixed inset-0 -z-0">
+    <div aria-hidden className="pointer-events-none fixed inset-0 z-0">
       <div className="absolute -top-32 right-0 h-72 w-72 md:h-[420px] md:w-[420px] rounded-full bg-sky-500/30 blur-3xl" />
       <div className="absolute bottom-0 -left-24 h-72 w-72 md:h-[420px] md:w-[420px] rounded-full bg-indigo-500/20 blur-3xl" />
     </div>
